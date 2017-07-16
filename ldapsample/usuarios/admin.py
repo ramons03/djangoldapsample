3ab3357de04e955319c7c08f085d6ba1f2e3cdec
root <root@vps-1279573-x.dattaweb.com>
2017-07-16 12:36:28 -0300

    commit inicial

---
+++ ldapsample/usuarios/admin.py
@@ -0,0 +1,26 @@
+#from django.contrib import admin
+
+# Register your models here.
+# -*- coding: utf-8 -*-
+# This software is distributed under the two-clause BSD license.
+# Copyright (c) The django-ldapdb project
+
+
+from django.contrib import admin
+from models import LdapGroup, LdapUser
+
+
+class LdapGroupAdmin(admin.ModelAdmin):
+    exclude = ['dn', 'usernames']
+    list_display = ['name', 'gid']
+    search_fields = ['name']
+
+
+class LdapUserAdmin(admin.ModelAdmin):
+    exclude = ['dn', 'password', 'photo']
+    list_display = ['username', 'first_name', 'last_name', 'email', 'uid']
+    search_fields = ['first_name', 'last_name', 'full_name', 'username']
+
+
+admin.site.register(LdapGroup, LdapGroupAdmin)
+admin.site.register(LdapUser, LdapUserAdmin)
---
+++ ldapsample/usuarios/models.py
@@ -0,0 +1,67 @@
+#from __future__ import unicode_literals
+
+#from django.db import models
+
+# Create your models here.
+# -*- coding: utf-8 -*-
+# This software is distributed under the two-clause BSD license.
+# Copyright (c) The django-ldapdb project
+
+from __future__ import unicode_literals
+
+from ldapdb.models.fields import (CharField, ImageField, ListField,
+                                  IntegerField)
+import ldapdb.models
+
+
+class LdapUser(ldapdb.models.Model):
+    """
+    Class for representing an LDAP user entry.
+    """
+    # LDAP meta-data
+    base_dn = "ou=people,dc=planetexpress,dc=com"
+    object_classes = ['inetOrgPerson']
+
+    # inetOrgPerson
+    first_name = CharField(db_column='givenName', verbose_name="Prime name")
+    last_name = CharField("Final name", db_column='sn')
+    full_name = CharField(db_column='cn')
+    email = CharField(db_column='mail')
+    phone = CharField(db_column='telephoneNumber', blank=True)
+    mobile_phone = CharField(db_column='mobile', blank=True)
+    photo = ImageField(db_column='jpegPhoto')
+
+    # posixAccount
+    uid = IntegerField(db_column='uidNumber', unique=True)
+    group = IntegerField(db_column='gidNumber')
+    gecos = CharField(db_column='gecos')
+    home_directory = CharField(db_column='homeDirectory')
+    login_shell = CharField(db_column='loginShell', default='/bin/bash')
+    username = CharField(db_column='uid', primary_key=True)
+    password = CharField(db_column='userPassword')
+
+    def __str__(self):
+        return self.username
+
+    def __unicode__(self):
+        return self.full_name
+
+
+class LdapGroup(ldapdb.models.Model):
+    """
+    Class for representing an LDAP group entry.
+    """
+    # LDAP meta-data
+    base_dn = "ou=people,dc=planetexpress,dc=com"
+    object_classes = ['inetOrgPerson']
+
+    # posixGroup attributes
+    gid = IntegerField(db_column='gidNumber', unique=True)
+    name = CharField(db_column='cn', max_length=200, primary_key=True)
+    usernames = ListField(db_column='memberUid')
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
